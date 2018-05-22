7ec917a2ab3fb1a49f9bd4cb586bc375b252519f
mmicr001 <mmicr001@odu.edu>
2018-05-22 18:20:30 -0400

    Added second player and block movement

--- canvas.js
+++ canvas.js
@@ -1,25 +1,41 @@
 function startGame(){
+	leftPlayer = new player(30, 90, 0, 0); 
+	rightPlayer = new player(30, 90, window.innerWidth-40, 0);
 	gameArea.initialize();
-	leftPlayer = new component(30, 90, 0, 0); 
+
 }
 
 var gameArea = {
 	canvas : document.createElement("canvas"),
 	initialize : function(){
-		this.canvas.width = window.innerWidth-100;
+		this.canvas.width = window.innerWidth;
 		this.canvas.height = window.innerHeight-100;
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.interval = setInterval(updateGameArea,20);
+	},
+	clear: function(){
+		this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
 	}
 }
 
-function component(w, h, x, y){
+function player(w, h, x, y){
 	this.width = w;
 	this.height = h;
 	this.x = x;
 	this.y = y;
-	ctx = gameArea.context;
-	ctx.fillStyle = "lime";
-	ctx.fillRect(this.x, this.y, this.width, this.height);
+	this.update = function(){
+		ctx = gameArea.context;
+		ctx.fillStyle = "lime";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+}
+
+function updateGameArea(){
+	gameArea.clear();
+	leftPlayer.y += 1;
+	rightPlayer.x -= 1;
+	leftPlayer.update();
+    rightPlayer.update();	
 }
 
