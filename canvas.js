f44daf76961fbc5c11be97f46abee84f6a8c73b9
mmicr001 <mmicr001@odu.edu>
2018-05-22 00:47:06 -0400

    Added left player block and Javascript constructor

--- canvas.js
+++ canvas.js
@@ -1,5 +1,6 @@
 function startGame(){
 	gameArea.initialize();
+	leftPlayer = new component(30, 90, 0, 0); 
 }
 
 var gameArea = {
@@ -10,5 +11,15 @@ var gameArea = {
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 	}
+}
+
+function component(w, h, x, y){
+	this.width = w;
+	this.height = h;
+	this.x = x;
+	this.y = y;
+	ctx = gameArea.context;
+	ctx.fillStyle = "lime";
+	ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
