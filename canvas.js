9effc98c2ce8f49789d13789797c7ca1b7c5508c
mmicr001 <mmicr001@odu.edu>
2018-05-23 18:22:51 -0400

    left block mirrors mouse height

--- canvas.js
+++ canvas.js
@@ -1,8 +1,8 @@
 function startGame(){
+	gameBall = ball(window.innerWidth/2, window.innerHeight/2);
 	leftPlayer = new player(30, 90, 0, 0); 
 	rightPlayer = new player(30, 90, window.innerWidth-40, 0);
 	gameArea.initialize();
-
 }
 
 var gameArea = {
@@ -19,22 +19,40 @@ var gameArea = {
 	}
 }
 
+function getMouseY(event){
+	leftPlayer.y = event.clientY-45;
+}
+ 
+function ball(x, y){
+	this.x = x;
+	this.y = y;
+	
+	ctx = gameArea.context;
+	//ctx.fillStyle = "lime";
+	
+	//ctx.beginPath();
+	//ctx.arc(this.x,this.y,50,0,2*Math.PI);
+}
+
 function player(w, h, x, y){
 	this.width = w;
 	this.height = h;
 	this.x = x;
 	this.y = y;
+	
 	this.update = function(){
 		ctx = gameArea.context;
 		ctx.fillStyle = "lime";
+		if(this.y < 0)
+			{this.y = 0;}
+		else if(this.y > gameArea.canvas.height - 90) 
+			{this.y = gameArea.canvas.height - 90;}
 		ctx.fillRect(this.x, this.y, this.width, this.height);
 	}
 }
 
 function updateGameArea(){
 	gameArea.clear();
-	leftPlayer.y += 1;
-	rightPlayer.x -= 1;
 	leftPlayer.update();
     rightPlayer.update();	
 }
