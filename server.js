2e382e221edbd13fb2623d303561899ee9d5bf49
mmicros <michael.micros@gmail.com>
2021-10-26 16:03:10 -0400

    fix horizontal bouncing

--- server.js
+++ server.js
@@ -7,15 +7,6 @@ var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
-// game variables
-var globals = {div: 20000,                                     // virtual grid size
-               dx: 200, dy: 0,                                 // arbitrary step size for ball movement
-               ballRadius: 500,                                // virtual ball radius (will be translated in canvas.js)
-               block: {width:20, height:100}};    // virtual block size (will be translated in canvas.js)
-
-/* console.log("sanity check");
-console.log("div = %i \ndx = %i \ndy = %i \nballRadius = %i \nblock = %i", 
-            globals.div, globals.dx, globals.dy, globals.ballRadius, globals.block); */
 app.set('port', 5000);
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/static', express.static(__dirname + '/static'));
@@ -30,23 +21,29 @@ server.listen(process.env.PORT || 5000, function() {
 });
 
 // *** GAME LOGIC ***
-
+// game variables
+var globals = {div: 20000,             // virtual grid size
+  dx: 80, dy: 0,                       // arbitrary step size for ball movement
+  ballRadius: 500,                     // virtual ball radius (will be translated in canvas.js)
+  block: {width:400, height:2000}};    // virtual block size (will be translated in canvas.js)
 var state = {};
-state['ball'] = {x:globals.div/2, y:globals.div/2, paused:0}; // div/2 puts the ball in the middle of the screen
-//state['blockHeight'] = 0; to delete
+state['ball'] = {x:globals.div/2, 
+                 y:globals.div/2,
+                 dx:globals.dx,
+                 dy:globals.dy,
+                 paused:0};
 state['players'] = {};
-state['mode'] = 0; // 0: menu, 1:single player, 2:multiplayer
-state['dims'] = {}; // width and height to be used for both player's canvases
+state['mode'] = 0;                     // 0: menu, 1:single player, 2:multiplayer
 var players = {}; 
 var id1,id2;
-
 
 io.on('connection', function(socket) 
 {	
   socket.on('new player', function() 
   { 
     console.log("player entered: " );
-  
+    
+    // first player to enter
     if(Object.keys(players).length == 0){
       players[socket.id] = { x:0, y:0, score:0};	
       id1 = socket.id;
@@ -55,6 +52,7 @@ io.on('connection', function(socket)
       io.sockets.emit('state', state);
     }
 
+    //second player to enter
     else if (Object.keys(players).length == 1){
       players[socket.id] = { x: dims.x, y:0, score:0};
       id2 = socket.id;
@@ -76,11 +74,11 @@ io.on('connection', function(socket)
   
   socket.on('state', function(data) {
     state.mode = data.mode;
-    //state.blockHeight = data.height;
     players[socket.id].y = data.y;
   });
 });
 
+// used after a player scores
 function pause(){
   setTimeout(function(){
     state.ball.paused = 0;
@@ -88,7 +86,6 @@ function pause(){
 }
 
 function leftHit(y){
-
   if( players[id1].y<y && y<(players[id1].y + globals.block.height) )
     return true;
   else
@@ -108,32 +105,33 @@ function rightHit(y){
 function updateBall(){
   x = state.ball.x;
   y = state.ball.y;
-  //console.log("ball = " + x + ", " + y);
-
-  if (x + globals.dx < globals.block.width+globals.ballRadius){
-    
+  nextX = x + globals.dx; // new positions after step is applied
+  nextY = y + globals.dy; //
+  
+  //check left boundary
+  if (nextX < globals.block.width+globals.ballRadius/2){
     if(leftHit(y)){
-      //console.log("block width = " + state.dims.x/40);
-
       globals.dx = -globals.dx; 
-      x -= globals.dx;
+      x = globals.ballRadius;
     }
     else{
-      x=globals.div/2; y=globals.div/2;
-      if(state.mode==2){
+      x=globals.div/2; 
+      y=globals.div/2;
+      if(state.mode==2)
         players[id2].score++;
-      }
       state.ball.paused=1; pause();
     }
   }
 
-  if (x + globals.dx > (state.mode==2 ? globals.div-globals.block.width : globals.div-globals.ballRadius)){
+  //check right boundary
+  if (nextX > (state.mode==2 ? globals.div-globals.block.width : globals.div-globals.ballRadius/2)){
     if(rightHit(y)){
       globals.dx = -globals.dx; 
-      x -= globals.dx;
+      x = globals.div-globals.block.width;
     }
     else{
-      x=globals.div/2; y=globals.div/2;
+      x=globals.div/2; 
+      y=globals.div/2;
       if(state.mode==2){
         players[id1].score++;
       }
@@ -141,34 +139,23 @@ function updateBall(){
     }
   }
 
-  //debug --- delete when done
-  /* if(y<1000 || y>19000)
-  {
-    console.log("ball is at %i", y);
-    console.log("new y = %i", y + globals.dy);
-  } */
-
-  // when ball hits bottom of canvas
-  if (y + globals.dy > globals.div-globals.ballRadius)
-  {
+  //check bottom boundary
+  if (nextY > globals.div-globals.ballRadius){
     y = globals.div - globals.ballRadius;
     globals.dy = -globals.dy; 
   }
 
-  // when ball hits top of canvas
-  else if(y + globals.dy < globals.ballRadius)
-  {
+  //check top boundary
+  else if(nextY < globals.ballRadius){
     y = globals.ballRadius;
     globals.dy = -globals.dy;     
   }
   
   // non edge cases
-  else
-  {
+  else{
     x += globals.dx;
     y += globals.dy;  
   }
-
 
   state.ball.x = x;
   state.ball.y = y;
@@ -182,4 +169,4 @@ setInterval(function() {
     state['players']= players; 
     io.sockets.emit('state', state);
   }
-}, 1000 / 60);
+}, 1000 / 10);
